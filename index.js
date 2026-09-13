d64cd441a020e001f70bb9bcdac9772f45985c7e
Mahmoud Aziz <41173335+mahmoudadelaziz@users.noreply.github.com>
2026-09-13 22:04:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = 8080
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
